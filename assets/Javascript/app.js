4ccfa3bac98ec3260683b9c7d4bb48311d626b61
Brandon Harrell <BHarrell@myrgroup.com>
2018-12-04 15:26:01 -0700

    function clock

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,11 +1,22 @@
 const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setdate() {
 
 const now = new Date();
+
 const seconds = now.getSeconds();
 const secondDegrees = ((seconds / 60) * 360) + 90;
 secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+const mins = now.getMinutes();
+const minsDegrees = ((mins / 60) * 360) + 90;
+minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+const hour = now.getHours();
+const hourDegrees = ((mins / 12) * 360) + 90;
+hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
 console.log(`%c color: red; ${seconds}`)
 }
