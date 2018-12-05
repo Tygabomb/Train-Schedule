98a8b2699077300dc9eda02833c89ad130612b6b
Tyga <b_harrell17@outlook.com>
2018-12-05 00:12:12 -0700

    js initial

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,3 +1,69 @@
+// conjuction function 
+
+let trainName = "";
+let destination = "";
+let trainTime = "";
+let freq = 0;
+let trainData = [];
+
+
+
+$("#submit").on("click", function(event)  {
+  event.preventDefault();
+
+  let trainName = $("#trainName").val().trim();
+  let destination = $("#trainDestination").val().trim();
+  let trainTime = moment($("#trainTime").val().trim());
+  let freq = $("#frequency").val().trim();
+
+  console.log(`${trainName}, ${lineName}, ${destination}, ${trainTime}, ${freq} `);
+    
+  trainData.push(trainName);
+
+
+  localStorage.setItem("train", JSON.stringify (trainName));
+  localStorage.setItem("city", JSON.stringify (destination));
+  localStorage.setItem("time", JSON.stringify (trainTime));
+  localStorage.setItem("freq", JSON.stringify (freq));
+
+  $("#trainName").val("");
+  $("#trainDestination").val("");
+  $("#trainTime").val("");
+  $("#frequency").val("");
+
+});
+
+// let trainData = JSON.parse(localStorage.getItem("trainData"))
+
+
+// let newRow = $("<tr>");
+//   newRow.append($("<td>" + localStorage.val().trainName + "</td>"));
+//   newRow.append($("<td>" + localStorage.val().destination + "</td>"));
+//   newRow.append($("<td class='text-center'>" + localStorage.val().frequency + "</td>"));
+//   newRow.append($("<td class='text-center'>" + moment(nextTrain).format("LTS") + "</td>"));
+//   newRow.append($("<td class='text-center'><button class='arrival btn btn-danger btn-xs' data-key='" + trainData + "'>X</button></td>"));
+
+
+
+// $(document).on("click", ".arrival", function() {
+//   keyref = $(this).attr("data-key");
+//   newRow.ref().child(keyref).remove();
+//   window.location.reload();
+// });
+
+
+
+
+  // var newTrain = {
+  //   name,  
+  //   line,
+  //   destination,
+  //   trainTime,
+  //   frequency,
+  //   Trains: ["trainName", "lineName", "destination", "trainTime", "frequency" ]
+  //
+
+// Clock const and functions 
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
@@ -22,3 +88,7 @@ console.log(`%c color: red; ${seconds}`)
 }
 
 setInterval(setdate, 1000);
+
+
+
+// 
