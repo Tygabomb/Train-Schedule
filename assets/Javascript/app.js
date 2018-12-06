cc3adee782f563001e7f3a8800992b78f3abece8
Tyga <b_harrell17@outlook.com>
2018-12-05 17:23:19 -0700

    close to finshed

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,3 +1,29 @@
+// Clock const and functions 
+  const secondHand = document.querySelector('.second-hand');
+  const minsHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
+
+  function setdate() {
+
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((mins / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+    console.log(` ${seconds}`)
+  }
+
+  setInterval(setdate, 1000);
+
 // conjuction function 
 
 let trainName = "";
@@ -6,66 +32,67 @@ let trainTime = "";
 let freq = 0;
 let trainData = [];
 let hhmmTime;
+let nextTrain;
 
-var milTime = moment().format("HH: mm");
-
+// let now = moment().format("HH: mm");
+// console.log(`${now}`);
 
 
 $("#submit").on("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    trainName = $("#trainName").val().trim();
-    destination = $("#trainDestination").val().trim();
-    trainTime = $("#trainTime").val().trim();
-    freq = $("#frequency").val().trim();
+  trainName = $("#trainName").val().trim();
+  destination = $("#trainDestination").val().trim();
+  trainTime = $("#trainTime").val().trim();
+  freq = $("#frequency").val().trim();
 
-    console.log(`${trainName}, ${lineName}, ${destination}, ${trainTime}, ${freq} `);
+  console.log(`${trainName} ${destination} ${trainTime} ${freq} `);
 
-    let hhmmTime = moment(trainTime, "h:mm  a A").format("HH:mm");
+  var hhmmTime = moment(trainTime, "h:mm  a A").format("HH:mm");
 
-    console.log(`${traindata}, ${hhmmTime}, `);
+  console.log(`${trainTime} ${hhmmTime} `);
+
+  nextTrain = moment(trainTime, 'HH:mm')
+  while (moment().isAfter(nextTrain, "HH:mm")) {
+    nextTrain = nextTrain.add(freq, 'm');
+    console.log(`${nextTrain}`);
+  }
 
 
-    
-    trainData.push({
-        name: trainName,
-        location: destination,
-        trainTime: trainTime,
-        time: freq,
-    });
 
-    // let trainData = {
-    //     name,
-    //     location,
-    //     destination,
-    //     trainTime,
-    //     frequency,
-    //     Trains: ["trainName", "lineName", "destination", "trainTime", "frequency"]
-    // };
+  trainData.push({
+    name: trainName,
+    location: destination,
+    trainTime: trainTime,
+    time: freq,
+  });
 
-    localStorage.setItem("train", JSON.stringify(trainData));
-    resetFields();
-    populate()
-    //   localStorage.setItem("city", JSON.stringify (destination));
-    //   localStorage.setItem("time", JSON.stringify (trainTime));
-    //   localStorage.setItem("freq", JSON.stringify (freq));
+  let trainData = {name: 'trainName', location:'destination', trainTime:'trainTime', time:'freq'};
 
-    function resetFields() {
-        $("#trainName").val("");
-        $("#trainDestination").val("");
-        $("#trainTime").val("");
-        $("#frequency").val("");
 
-    };
+  localStorage.setItem("trainSchedule", JSON.stringify(trainData));
+  resetFields();
+  populate(nextTrain)
+  //   localStorage.setItem("city", JSON.stringify (destination));
+  //   localStorage.setItem("time", JSON.stringify (trainTime));
+  //   localStorage.setItem("freq", JSON.stringify (freq));
 
-    function populate(militaryTime, nextArrival) {
-        // createRow on the table;
-        var duration = moment.duration(moment(nextArrival, "HH:mm").diff(moment())).asMinutes()
-        var minutesTill = Math.round(duration);
-        console.log(duration);
-        var tBody = $("tbody");
-        var tRow = $("<tr>");
-    
+  function resetFields() {
+    $("#trainName").val("");
+    $("#trainDestination").val("");
+    $("#trainTime").val("");
+    $("#frequency").val("");
+
+  }
+
+  function populate(hhmmTime) {
+    // new div infro 
+    var duration = moment.duration(moment(nextArrival, "HH:mm").diff(moment())).asMinutes()
+    var minutesTill = Math.round(duration);
+    console.log(duration);
+    var tBody = $("tbody");
+    var tRow = $("<tr>");
+
 
     var trainNa = $("<td>").text(trainName);
     var dest = $("<td>").text(destination);
@@ -73,7 +100,7 @@ $("#submit").on("click", function (event) {
     var trainSchedule = $("<td>").text(nextArrival);
     var minutesAway = $("<td>").text(minutesTill);
 
-   
+
 
 
     // let newRow = $("<tr>");
@@ -95,35 +122,8 @@ $("#submit").on("click", function (event) {
     tBody.append(tRow);
 
 
-    }
-
-
-    // Clock const and functions 
-    const secondHand = document.querySelector('.second-hand');
-    const minsHand = document.querySelector('.min-hand');
-    const hourHand = document.querySelector('.hour-hand');
-
-    function setdate() {
-
-        const now = new Date();
-
-        const seconds = now.getSeconds();
-        const secondDegrees = ((seconds / 60) * 360) + 90;
-        secondHand.style.transform = `rotate(${secondDegrees}deg)`;
-
-        const mins = now.getMinutes();
-        const minsDegrees = ((mins / 60) * 360) + 90;
-        minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
-        const hour = now.getHours();
-        const hourDegrees = ((mins / 12) * 360) + 90;
-        hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
-        console.log(`%c color: red; ${seconds}`)
-    }
-
-    setInterval(setdate, 1000);
-
-
-
+  };
 });
+
+
+  
